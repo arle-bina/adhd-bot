d259ca79c1f7dd93a4f0b75d51aadd0168073207
Egg <jlpower112@gmail.com>
2026-03-10 23:54:51 -0400

    feat: register all new commands with Discord API

--- src/register.ts
+++ src/register.ts
@@ -1,10 +1,22 @@
 import { REST, Routes } from "discord.js";
 import { validateEnv } from "./utils/env.js";
 import * as profileCommand from "./commands/profile.js";
+import * as leaderboardCommand from "./commands/leaderboard.js";
+import * as partyCommand from "./commands/party.js";
+import * as electionsCommand from "./commands/elections.js";
+import * as stateCommand from "./commands/state.js";
+import * as newsCommand from "./commands/news.js";
 
 validateEnv();
 
-const commands = [profileCommand.data.toJSON()];
+const commands = [
+  profileCommand.data.toJSON(),
+  leaderboardCommand.data.toJSON(),
+  partyCommand.data.toJSON(),
+  electionsCommand.data.toJSON(),
+  stateCommand.data.toJSON(),
+  newsCommand.data.toJSON(),
+];
 
 const rest = new REST().setToken(process.env.DISCORD_BOT_TOKEN!);
 
